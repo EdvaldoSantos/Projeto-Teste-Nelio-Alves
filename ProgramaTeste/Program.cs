7a6c83873e1729f01217f10ccac06fb7050dbab6
Edvaldo Santos <edvaldonsantos86@gmail.com>
2020-11-29 06:43:20 -0300

    Projeto iniciado, mostrando hello world

---
+++ ProgramaTeste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProgramaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
