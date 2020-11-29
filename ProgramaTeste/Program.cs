3b70695813c67790c11fb674146c3171db6bbe52
Edvaldo Santos <edvaldonsantos86@gmail.com>
2020-11-29 07:51:56 -0300

    Perguntando que dia e hoje

--- ProgramaTeste/Program.cs
+++ ProgramaTeste/Program.cs
@@ -9,6 +9,7 @@ namespace ProgramaTeste
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está?");
+            Console.WriteLine("Que dia é hoje!!!!" );
             Console.ReadKey();
         }
     }
